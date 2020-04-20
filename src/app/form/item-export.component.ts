46ff6dc38130d6a472629580c480196cc0cb7037
Carlo Field <carlo@swiss.dev>
2020-04-20 17:01:52 +0200

    style item exports

--- src/app/form/item-export.component.ts
+++ src/app/form/item-export.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewChecked, AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Item } from '../model';
 
@@ -6,23 +6,39 @@ import { Item } from '../model';
   selector: 'bls-item-export',
   template: `
     <mat-dialog-content>
-      <pre>
-        bl3({{data.wrapper.item_serial_number}})
-      </pre>
+      <mat-form-field style="width: 100%;">
+        <textarea rows="5" readonly matInput name="code"
+                  (click)="box.select()" #box class="code">bl3({{data.wrapper.item_serial_number}})</textarea>
+      </mat-form-field>
     </mat-dialog-content>
     <mat-dialog-actions>
-      <button matDialogClose>Close</button>
+      <button mat-raised-button matDialogClose>Close</button>
     </mat-dialog-actions>
   `,
-  styles: []
+  styles: [
+    `
+    .code {
+      font-family: Consolas,monospace;
+      background-color: rgba(0, 0, 0, 0.1);
+    }
+    `
+  ]
 })
-export class ItemExportComponent implements OnInit {
+export class ItemExportComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('box')
+  box: ElementRef<HTMLTextAreaElement>;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: Item
   ) { }
 
   ngOnInit(): void {
+  }
+
+  public ngAfterViewInit(): void {
+    this.box.nativeElement.focus();
+    this.box.nativeElement.select();
   }
 
 }
