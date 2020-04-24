b3a1f021553b7ecc7a5c177da29519f0926f0d84
Carlo Field <carlo@swiss.dev>
2020-04-24 09:17:52 +0200

    make inv_raw lf update minimum version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   proxy: 'http://localhost:5050',
-  minimumVersion: 'v1.0.6'
+  minimumVersion: 'v1.0.10'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   proxy: 'http://localhost:5050',
-  minimumVersion: 'v1.0.6'
+  minimumVersion: 'v1.0.10'
 };
 
 /*
